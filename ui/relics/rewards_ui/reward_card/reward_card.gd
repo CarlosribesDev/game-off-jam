d30f17453a96de7a7c6d0e7137dd9ffc72a5e4b9
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-16 18:30:41 +0100

    fix cards

--- ui/relics/rewards_ui/reward_card/reward_card.gd
+++ ui/relics/rewards_ui/reward_card/reward_card.gd
@@ -6,17 +6,15 @@ var relic: Relic
 
 @onready var description: RichTextLabel = $MarginContainer/MarginContainer/VBoxContainer/Description
 @onready var title: Label = $MarginContainer/MarginContainer/VBoxContainer/Title
-@onready var texture_button: TextureButton = $TextureButton
+@onready var relic_texture: TextureRect = $RelicTexture
 
 func set_relic(new_relic_value: Relic) -> void:
 	relic = new_relic_value
-	texture_button.texture_normal = relic.get_texture()
+	relic_texture.texture = relic.get_texture()
 	title.text = relic.get_id()
 	description.text = relic.get_description()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-func _on_texture_button_pressed() -> void:
-	card_pressed.emit(relic)
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
+			card_pressed.emit(relic)
