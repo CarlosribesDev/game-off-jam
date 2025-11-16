803a79fbc93c66582ee7afbe4ca7991c9d0d3dff
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-16 18:52:26 +0100

    lives

--- ui/score_ui/lives_counter/lives_counter.gd
+++ ui/score_ui/lives_counter/lives_counter.gd
@@ -2,6 +2,10 @@ class_name LivesCounter extends Control
 
 @onready var couner_label: Label = $VBoxContainer/CounerLabel
 
+
+func _ready() -> void:
+	Score.lives_change.connect(func(value): lives = value)
+
 var lives: int:
 	set(value):
 		lives = value
