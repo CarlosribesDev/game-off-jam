5f5cd83bb9e1cf2f4366866777f8121bedb4c0ff
Angelet-code <sagara.ad@gmail.com>
2025-11-20 01:37:03 +0100

    separación wavescounter

---
+++ ui/top_bar_ui/waves_ui/waves_counter_ui.gd
@@ -0,0 +1,25 @@
+class_name WavesCounterUi extends Control
+
+@export var enemy_generator: EnemyGenerator
+
+@onready var waves_counter_current: Label = $HBoxContainer/WavesCounterCurrent
+@onready var waves_counter_total: Label = $HBoxContainer/WavesCounterTotal
+
+var _total_waves: int = 0
+var _current_wave: int = 0
+
+func _ready():
+	enemy_generator.wave_change.connect(_update_waves_counter)
+	enemy_generator.new_level_loaded.connect(_on_new_level)
+	
+func _update_total_waves(new_value: int) -> void:
+	waves_counter_total.text = str(new_value)
+	_total_waves = new_value
+
+func _update_waves_counter(new_value: int) -> void:
+	waves_counter_current.text = str(new_value)
+	_current_wave = new_value
+
+func _on_new_level() -> void:
+	var total_waves = enemy_generator.total_waves
+	_update_total_waves(total_waves)
--- ui/top_bar_ui/waves_ui/waves_ui.gd
+++ ui/top_bar_ui/waves_ui/waves_ui.gd
@@ -5,8 +5,6 @@ signal next_wave()
 @export var enemy_generator: EnemyGenerator
 
 @onready var next_wave_container: VBoxContainer = $CenterContainer/NextWaveContainer
-@onready var waves_counter_current: Label = $WavesCounterContainer/MarginContainer/HBoxContainer/WavesCounterCurrent
-@onready var waves_counter_total: Label = $WavesCounterContainer/MarginContainer/HBoxContainer/WavesCounterTotal
 @onready var next_wave_button: Button = $CenterContainer/NextWaveContainer/NextWaveButton
 
 var _total_waves: int = 0
@@ -18,11 +16,9 @@ func _ready():
 	enemy_generator.new_level_loaded.connect(_on_new_level)
 	
 func _update_total_waves(new_value: int) -> void:
-	waves_counter_total.text = str(new_value)
 	_total_waves = new_value
 
 func _update_waves_counter(new_value: int) -> void:
-	waves_counter_current.text = str(new_value)
 	_current_wave = new_value
 
 func _on_wave_finished() -> void:
@@ -35,7 +31,6 @@ func _on_next_wave_button_pressed() -> void:
 	
 func _on_new_level() -> void:
 	var total_waves = enemy_generator.total_waves
-	
 	_update_total_waves(total_waves)
 	_show_next_wave_button()
 	
