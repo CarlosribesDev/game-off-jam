ff328659b035f7cadd457471aa5733708db4bdff
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-25 20:07:22 +0100

    more menu changes

--- ui/pause_ui/pause_menu.gd
+++ ui/pause_ui/pause_menu.gd
@@ -1,4 +1,6 @@
-extends Control
+class_name PauseMenu extends Control
+
+const MAIN_MENU = preload("uid://4i6kl0xurgeg")
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
@@ -17,9 +19,14 @@ func testEsc():
 	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
 		resume()
 
+
 func _on_resume_pressed() -> void:
 	resume()
 
 func _on_restart_pressed() -> void:
 	resume()
 	get_tree().root.get_node("Game").reset_current_level()
+
+func _on_exit_button_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_packed(MAIN_MENU)
